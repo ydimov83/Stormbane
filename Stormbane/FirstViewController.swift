da7b5856fd6301e26661a7bf1615ed14b6b524a9
Yavor Dimov <ydimov@gmail.com>
2019-05-10 15:11:38 -0600

    Initial Commit

---
+++ Stormbane/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Stormbane
+//
+//  Created by Yavor Dimov on 5/10/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Stormbane/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Stormbane
+//
+//  Created by Yavor Dimov on 5/10/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
